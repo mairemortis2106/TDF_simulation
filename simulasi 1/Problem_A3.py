3c39fc7c2b9415440c660bb98317f75571fb4e4c
mairemortis2106 <michaelalam2106@gmail.com>
2025-06-06 11:40:50 +0700

    update

--- simulasi 1/Problem_A3.py
+++ simulasi 1/Problem_A3.py
@@ -1,96 +1,68 @@
-# ======================================================================================================
-# PROBLEM A3
-#
-# Build a classifier for the Human or Horse Dataset with Transfer Learning.
-# The test will expect it to classify binary classes.
-# Note that all the layers in the pre-trained model are non-trainable.
-# Do not use lambda layers in your model.
-#
-# The horse-or-human dataset used in this problem is created by Laurence Moroney (laurencemoroney.com).
-# Inception_v3, pre-trained model used in this problem is developed by Google.
-#
-# Desired accuracy and validation_accuracy > 97%.
-# =======================================================================================================
+suara = [0, 0, 0, 0, 0, 0, 0]
+total = 0
+naik = 0
+turun = 0
+konstan = 0
 
-import urllib.request
-import zipfile
-import tensorflow as tf
-from keras_preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras import layers
-from tensorflow.keras import Model
-from tensorflow.keras.applications.inception_v3 import InceptionV3
+for i in range(7):
+    suara[i] = int(input(f"Masukkan suara pada hari ke-{i + 1}:"))
+    if suara[i] >= 80:
+        waktu = int(input("Berapa lama suara(jam):"))
+        if waktu >= 8:
+            menjauh = (suara[i] - 80)
+            total += suara[i]
+            print(
+                f"Peringatan suara besar dan kontinu, segera menjauh {menjauh} m dari sumber suara pada hari ke-{i + 1}")
+            print("")
+        else:
+            total += suara[i]
+            print(f"Peringatan suara besar dan diskontinu pada hari ke-{i + 1}")
+            print("_")
+    elif suara[i] >= 1 and suara[i] < 80:
+        waktu = int(input("Berapa lama suara(jam):"))
+        if waktu >= 4:
+            total += suara[i]
+            print(f"Peringatan suara kecil dan kontinu pada hari ke-{i + 1}")
+            print("")
+        else:
+            print(f"Tidak ada peringatan pada hari ke-{i + 1}")
+            print("")
+    else:
+        print("Tidak terdapat suara")
+        print("")
 
+for i in range(7):
+    if suara[i] > suara[i + 1]:
+        turun += 1
+        print(f"Desibel suara menurun pada hari ke-{i + 2}")
+    elif suara[i] < suara[i + 1]:
+        naik += 1
+        print(f"Desibel suara naik pada hari ke-{i + 2}")
+    else:
+        konstan += 1
+        print(f"Desibel suara konstan pada hari ke-{i + 2}")
+    if i == 5:
+        break
 
-def solution_A3():
-    inceptionv3 = 'https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    urllib.request.urlretrieve(
-        inceptionv3, 'inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
-    local_weights_file = 'inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+if naik > turun:
+    print("Desibel relatif meningkat dalam seminggu")
 
-    pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
-                                    include_top=False,
-                                    weights=local_weights_file)
+elif naik < turun:
+    print("Desibel suara relatif menurun dalam seminggu")
 
-    for layer in pre_trained_model.layers:
-        layer.trainable = False
+else:
+    print("Desibel suara relatif konstan dalam seminggu")
 
-    last_layer = pre_trained_model.get_layer('mixed7')
+makshimum = suara[0]
+minumum = suara[0]
 
-    data_url_1 = 'https://github.com/dicodingacademy/assets/releases/download/release-horse-or-human/horse-or-human.zip'
-    urllib.request.urlretrieve(data_url_1, 'horse-or-human.zip')
-    local_file = 'horse-or-human.zip'
-    zip_ref = zipfile.ZipFile(local_file, 'r')
-    zip_ref.extractall('data/horse-or-human')
-    zip_ref.close()
+for i in range(1, 7):
+    if suara[i] > makshimum:
+        makshimum = suara[i]
+    if suara[i] < minumum:
+        minumum = suara[i]
 
-    data_url_2 = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/validation-horse-or-human.zip'
-    urllib.request.urlretrieve(data_url_2, 'validation-horse-or-human.zip')
-    local_file = 'validation-horse-or-human.zip'
-    zip_ref = zipfile.ZipFile(local_file, 'r')
-    zip_ref.extractall('data/validation-horse-or-human')
-    zip_ref.close()
-
-    train_dir = 'data/horse-or-human'
-    validation_dir = 'data/validation-horse-or-human'
-
-    train_datagen = ImageDataGenerator(
-        rescale = 1./255,
-        rotation_range = 40,
-        width_shift_range = 0.2,
-        height_shift_range = 0.2,
-    )
-    valid_datagen = ImageDataGenerator(
-        rescale=1 / 255
-    )
-    # YOUR IMAGE SIZE SHOULD BE 150x150
-    train_generator = train_datagen.flow_from_directory(train_dir,
-                                                        target_size=(150, 150),
-                                                        color_mode='rgb',
-                                                        class_mode='binary')
-    valid_generator = valid_datagen.flow_from_directory(validation_dir,
-                                                        target_size=(150, 150),
-                                                        color_mode='rgb',
-                                                        class_mode='binary')
-    x = last_layer.output
-    x = layers.Flatten()(x)
-    x = layers.Dense(16,activation='relu')(x)
-    x = layers.Dropout(rate=0.2)(x)
-    x = layers.Dense(1, activation='sigmoid')(x)
-
-    model = Model(pre_trained_model.input, x)
-
-    model.compile(optimizer=RMSprop(lr=0.0001),
-                  loss='binary_crossentropy',
-                  metrics=['acc'])
-    model.fit(train_generator, validation_data=valid_generator, epochs=10)
-
-    return model
-
-
-# The code below is to save your model as a .h5 file.
-# It will be saved automatically in your Submission folder.
-if __name__ == '__main__':
-    # DO NOT CHANGE THIS CODE
-    model = solution_A3()
-    model.save("model_A3.h5")
+print(f"Nilai suara maksimum:{makshimum} db")
+print(f"nilai suara minimum adalah {minumum} db")
+rata = total / 7
+print(f"Rata-rata suara dalam interval satu minggu adalah {rata} db")
